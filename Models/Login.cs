06ba50fb87260495167b3f9fc607ac70af2b3f7c
Hobey Padlan <hobeypadlanxx@gmail.com>
2023-06-04 01:27:53 +0800

    Login model

---
+++ Models/Login.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace fur_ever_homes.Models
+{
+    public class Login
+    {
+        [Required]
+        [MinLength(3)]
+        [MaxLength(30)]
+        public string username { get; set; }
+    }
+}
