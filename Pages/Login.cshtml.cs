c41706efc941d746460c0143d3680e5b10722810
Hobey Padlan <hobeypadlanxx@gmail.com>
2023-06-04 17:14:57 +0800

    did stuff

--- Pages/Login.cshtml.cs
+++ Pages/Login.cshtml.cs
@@ -1,3 +1,4 @@
+using fur_ever_homes.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +6,22 @@ namespace fur_ever_homes.Pages
 {
     public class LoginModel : PageModel
     {
+        [BindProperty]
+        public LogIn LogIn { get; set; } = default!;
+
         public void OnGet()
         {
+        }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid || LogIn == null)
+            {
+                return Page();
+            }
+
+            HttpContext.Session.SetString("LogInState", "true");
+            return RedirectToPage("Index");
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -3,6 +3,15 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 
+builder.Services.AddDistributedMemoryCache();
+
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(30);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
